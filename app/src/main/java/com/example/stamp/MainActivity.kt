2bdf317662ad5de2cec1be7f2fa53be1c882e4b0
uknow <yoon6763@naver.com>
2022-09-14 08:14:29 +0900

    Map Coloring Sample Source Add

--- app/src/main/java/com/example/stamp/MainActivity.kt
+++ app/src/main/java/com/example/stamp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.stamp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.stamp.databinding.ActivityMainBinding
+import com.richpath.RichPath
+
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        binding.run {
+
+
+
+            val cheonan = ivKorea.findRichPathByName("충남 천안")!!
+            cheonan.fillColor = getColor(R.color.teal_700)
+
+        }
     }
 }
