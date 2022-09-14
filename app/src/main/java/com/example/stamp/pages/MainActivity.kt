f9ed7484ce939c5719e3b58ab34510ec6a0268d3
전윤호@랩실컴 <yoon6763@naver.com>
2022-09-14 13:46:07 +0900

    Image drawable Source Add and BottomNav Init

---
+++ app/src/main/java/com/example/stamp/pages/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.stamp.pages
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.stamp.R
+import com.example.stamp.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+        binding.run {
+            //val cheonan = ivKorea.findRichPathByName("충남 천안")!!
+            //cheonan.fillColor = getColor(R.color.teal_700)
+            supportFragmentManager.beginTransaction().add(R.id.container_main, HomeFragment()).commit()
+
+            navMain.setOnItemSelectedListener {
+                when(it.itemId) {
+                    R.id.nav_home -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.container_main, HomeFragment()).commit()
+                        return@setOnItemSelectedListener true
+                    }
+
+                    R.id.nav_map -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.container_main, MapFragment()).commit()
+                        return@setOnItemSelectedListener true
+                    }
+
+                    R.id.nav_feed -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.container_main, FeedFragment()).commit()
+                        return@setOnItemSelectedListener true
+                    }
+
+                    R.id.nav_my_page -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.container_main, MyPageFragment()).commit()
+                        return@setOnItemSelectedListener true
+                    }
+                }
+                return@setOnItemSelectedListener false
+            }
+
+        }
+    }
+}
