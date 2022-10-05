a08249519d58f4a16f093f47406a257b3a48c3c7
915_랩실컴 <yoon6763@naver.com>
2022-10-05 15:54:19 +0900

    파이어베이스 패키지 적용

--- app/src/main/java/com/amazing/stamp/pages/session/RegisterActivity.kt
+++ app/src/main/java/com/amazing/stamp/pages/session/RegisterActivity.kt
@@ -3,13 +3,21 @@ package com.amazing.stamp.pages.session
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.stamp.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+    private var auth: FirebaseAuth? = null
     private val binding by lazy { ActivityRegisterBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        auth = Firebase.auth
 
         setSupportActionBar(binding.toolbarRegister)
         supportActionBar?.run {
@@ -18,8 +26,49 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         binding.run {
-            btnRegisterFinish.setOnClickListener { finish() }
+            btnRegisterFinish.setOnClickListener { onRegister() }
         }
+    }
+
+    private fun onRegister() {
+        binding.run {
+            val email = etEmail.text.toString()
+            val password = etPw.text.toString()
+            val passwordCheck = etPwCheck.text.toString()
+            val nickname = etNickname.text.toString()
+
+            if (email.isEmpty()) {
+                Toast.makeText(applicationContext, "이름을 입력해주세요", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(applicationContext, "비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            if (password != passwordCheck) {
+                Toast.makeText(applicationContext, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            if (nickname.isEmpty()) {
+                Toast.makeText(applicationContext, "닉네임을 입력해주세요", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            auth?.createUserWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener(this@RegisterActivity) { task ->
+                    if(task.isSuccessful) {
+                        Toast.makeText(applicationContext, "계정이 생성되었습니다", Toast.LENGTH_SHORT).show()
+                        finish()
+                    } else {
+                        Toast.makeText(applicationContext, "계정 생성에 실패하였습니다",Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }
+
     }
 
 
