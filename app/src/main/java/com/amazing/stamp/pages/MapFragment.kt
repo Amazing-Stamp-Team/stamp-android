3166ebd99c65bb67139a7180e805d6514d2a1607
uknow <yoon6763@naver.com>
2022-10-08 23:58:27 +0900

    svg 벡터 파일 path 색상 설정 방식을 RichPath -> VectorChildFinder 방식으로 변경

--- app/src/main/java/com/amazing/stamp/pages/MapFragment.kt
+++ app/src/main/java/com/amazing/stamp/pages/MapFragment.kt
@@ -1,21 +1,24 @@
 package com.amazing.stamp.pages
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.devs.vectorchildfinder.VectorChildFinder
 import com.example.stamp.R
+import com.example.stamp.databinding.FragmentFeedBinding
 import com.example.stamp.databinding.FragmentMapBinding
+import com.richpath.RichPathView
 
 
 class MapFragment : Fragment() {
-
-    private val binding by lazy { FragmentMapBinding.inflate(layoutInflater) }
+    private lateinit var binding: FragmentMapBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -23,16 +26,27 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        binding.run {
-            val cheonan = ivKorea.findRichPathByName("충남 천안")!!
-            cheonan.fillColor = requireActivity().getColor(R.color.teal_700)
+        binding = FragmentMapBinding.inflate(inflater, container, false)
 
-            val  yesan = ivKorea.findRichPathByName("충남_예산군")!!
-            yesan.fillColor = requireActivity().getColor(R.color.purple_700)
-        }
 
+        val vector =
+            VectorChildFinder(requireContext(), R.drawable.ic_img_korea_detail, binding.ivKorea)
+        val seoul = vector.findPathByName("충남 천안")
+        seoul.fillColor = Color.RED
+
+        binding.ivKorea.invalidate()
+
+
+//        binding.run {
+//            val cheonan = ivKorea.findRichPathByName("충남 천안")!!
+//            cheonan.fillColor = requireActivity().getColor(R.color.teal_700)
+//
+//            val yesan = ivKorea.findRichPathByName("충남_예산군")!!
+//            yesan.fillColor = requireActivity().getColor(R.color.purple_700)
+//        }
 
         return binding.root
+//        return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
 }
