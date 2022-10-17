de815ee28c226145e3ad6d8e389e0a307f1fd12f
uknow <yoon6763@naver.com>
2022-10-18 00:29:40 +0900

    유저 검색 기능 추가 및 파이어베이스 - RecyclerAdapter 검색

--- app/src/main/java/com/amazing/stamp/adapter/FriendAddAdapter.kt
+++ app/src/main/java/com/amazing/stamp/adapter/FriendAddAdapter.kt
@@ -6,11 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.amazing.stamp.models.FriendAddModel
+import com.amazing.stamp.models.UserModel
+import com.amazing.stamp.utils.FirebaseConstants
 import com.example.stamp.R
 import com.example.stamp.databinding.ItemFriendsAddBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 
-class FriendAddAdapter(val context: Context, private val models: ArrayList<FriendAddModel>) :
+class FriendAddAdapter(val context: Context, val fireStore: FirebaseFirestore?, private val models: ArrayList<UserModel>) :
     RecyclerView.Adapter<FriendAddAdapter.Holder>() {
+
+    fun fireStoreSearch(keyword:String) {
+        fireStore?.collection(FirebaseConstants.COLLECTION_USERS)?.addSnapshotListener { value, error ->
+            models.clear()
+
+            for(snapshot in value!!.documents) {
+                if(snapshot.getString(FirebaseConstants.USER_FIELD_NICKNAME)!!.contains(keyword)) {
+                    val item = snapshot.toObject<UserModel>()
+                    models.add(item!!)
+                }
+            }
+            notifyDataSetChanged()
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -21,11 +39,11 @@ class FriendAddAdapter(val context: Context, private val models: ArrayList<Frien
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.run {
-            binding.tvFriendsName.text = models[position].name
+            binding.tvFriendsName.text = models[position].nickname
             binding.tvFriendsEmail.text = models[position].email
 
-            if (models[position].image != null) {
-                binding.ivFriendsProfile.setImageBitmap(models[position].image)
+            if (models[position].imageName != null) {
+                //binding.ivFriendsProfile.setImageBitmap(models[position].image)
             }
         }
     }
--- app/src/main/java/com/amazing/stamp/pages/sns/FriendsSearchActivity.kt
+++ app/src/main/java/com/amazing/stamp/pages/sns/FriendsSearchActivity.kt
@@ -2,35 +2,43 @@ package com.amazing.stamp.pages.sns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
 import com.amazing.stamp.adapter.FriendAddAdapter
 import com.amazing.stamp.models.FriendAddModel
+import com.amazing.stamp.models.UserModel
 import com.example.stamp.R
 import com.example.stamp.databinding.ActivityFriendsSearchBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class FriendsSearchActivity : AppCompatActivity() {
     private val binding by lazy { ActivityFriendsSearchBinding.inflate(layoutInflater) }
+    private val friendsList = ArrayList<UserModel>()
     private lateinit var friendAdapter: FriendAddAdapter
-    private val friendsList = ArrayList<FriendAddModel>()
+    private var fireStore: FirebaseFirestore? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        pushSampleData()
+        fireStore = Firebase.firestore
+
         setUpFriendRecyclerView()
+        setUpSearchOption()
     }
 
-    private fun pushSampleData() {
-        friendsList.add(FriendAddModel("홍길동", "asdf@asd.sd", null))
-        friendsList.add(FriendAddModel("홍길동", "asdf@asd.sd", null))
-        friendsList.add(FriendAddModel("홍길동", "asdf@asd.sd", null))
-        friendsList.add(FriendAddModel("홍길동", "asdf@asd.sd", null))
-    }
 
     private fun setUpFriendRecyclerView() {
-        friendAdapter = FriendAddAdapter(applicationContext, friendsList)
+        friendAdapter = FriendAddAdapter(applicationContext, fireStore, friendsList)
         binding.rvFriends.adapter = friendAdapter
         friendAdapter.notifyDataSetChanged()
+    }
+
+    private fun setUpSearchOption() {
+        binding.etFriendsSearch.addTextChangedListener {
+            friendAdapter.fireStoreSearch(it.toString())
+        }
     }
 
     override fun onBackPressed() {
