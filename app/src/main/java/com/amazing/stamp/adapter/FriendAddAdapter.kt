120ae0fd6775096d39aa2708f41b4ff0865d0c99
uknow <yoon6763@naver.com>
2022-10-31 00:19:39 +0900

    FriendsSearchActivity를 상속하여 게시물 작성에서 친구를 태그하는 FriendTagActivity 추가 및 리스트 클릭 시 메소드 오버라이딩

--- app/src/main/java/com/amazing/stamp/adapter/FriendAddAdapter.kt
+++ app/src/main/java/com/amazing/stamp/adapter/FriendAddAdapter.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.amazing.stamp.models.FriendAddModel
 import com.amazing.stamp.models.UserModel
 import com.amazing.stamp.utils.FirebaseConstants
 import com.example.stamp.R
@@ -15,7 +14,6 @@ import com.example.stamp.databinding.ItemFriendsAddBinding
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.storage.FirebaseStorage
-import kotlinx.coroutines.tasks.await
 
 class FriendAddAdapter(
     val context: Context,
@@ -25,21 +23,28 @@ class FriendAddAdapter(
 ) :
     RecyclerView.Adapter<FriendAddAdapter.Holder>() {
 
+    var idx = 0
+    private var profileArrayList = ArrayList<ByteArray?>()
+
     interface ItemClickListener {
-        fun onItemClick(userModel: UserModel)
+        fun onItemClick(profile:ByteArray?, userModel: UserModel)
     }
 
     lateinit var itemClickListener: ItemClickListener
 
     fun fireStoreSearch(keyword: String) {
+        idx++
+
         fireStore?.collection(FirebaseConstants.COLLECTION_USERS)
             ?.addSnapshotListener { value, error ->
                 models.clear()
+                profileArrayList.clear()
 
                 for (snapshot in value!!.documents) {
                     if (snapshot.getString(FirebaseConstants.USER_FIELD_NICKNAME)!!.contains(keyword)) {
                         val item = snapshot.toObject<UserModel>()
                         models.add(item!!)
+                        profileArrayList.add(null)
                     }
                 }
                 notifyDataSetChanged()
@@ -59,7 +64,7 @@ class FriendAddAdapter(
             binding.tvFriendsEmail.text = models[position].email
 
             if (models[position].imageName != null && models[position].imageName != "") {
-                setUserImage(binding, models[position])
+                setUserImage(binding, idx, models[position], position)
                 //binding.ivFriendsProfile.setImageBitmap(models[position].image)
             } else {
                 binding.ivFriendsProfile.setImageBitmap(null)
@@ -67,21 +72,24 @@ class FriendAddAdapter(
         }
     }
 
-    private fun setUserImage(binding: ItemFriendsAddBinding, userModel: UserModel) {
-        val gsReference =
-            storage!!.getReference("${FirebaseConstants.STORAGE_PROFILE}/${userModel!!.imageName!!}")
+    private fun setUserImage(binding: ItemFriendsAddBinding, idx:Int, userModel: UserModel, position: Int) {
+        val gsReference = storage!!.getReference("${FirebaseConstants.STORAGE_PROFILE}/${userModel!!.imageName!!}")
 
         gsReference.getBytes(FirebaseConstants.TEN_MEGABYTE).addOnCompleteListener {
             if(it.isSuccessful) {
-                val bmp = BitmapFactory.decodeByteArray(it.result, 0, it.result.size)
-                binding.ivFriendsProfile.setImageBitmap(
-                    Bitmap.createScaledBitmap(
-                        bmp,
-                        binding.ivFriendsProfile.width,
-                        binding.ivFriendsProfile.height,
-                        false
+                if(idx == this.idx) {
+                    val bmp = BitmapFactory.decodeByteArray(it.result, 0, it.result.size)
+                    profileArrayList[position] = it.result
+
+                    binding.ivFriendsProfile.setImageBitmap(
+                        Bitmap.createScaledBitmap(
+                            bmp,
+                            binding.ivFriendsProfile.width,
+                            binding.ivFriendsProfile.height,
+                            false
+                        )
                     )
-                )
+                }
             }
         }
     }
@@ -95,7 +103,7 @@ class FriendAddAdapter(
         val binding = ItemFriendsAddBinding.bind(itemView)
         init {
             itemView.setOnClickListener {
-                itemClickListener.onItemClick(models[adapterPosition])
+                itemClickListener.onItemClick(profileArrayList[adapterPosition], models[adapterPosition])
             }
         }
     }
