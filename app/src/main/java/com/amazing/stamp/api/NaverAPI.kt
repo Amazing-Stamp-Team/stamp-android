e79394f0f031b0415485c86799a7e27035c07db7
uknow <yoon6763@naver.com>
2022-11-22 01:28:01 +0900

    네이버지도 검색 API 개선

--- app/src/main/java/com/amazing/stamp/api/NaverAPI.kt
+++ app/src/main/java/com/amazing/stamp/api/NaverAPI.kt
@@ -2,12 +2,10 @@ package com.amazing.stamp.api
 
 import com.amazing.stamp.api.dto.NaverMapSearchResponseDTO
 import com.amazing.stamp.utils.SecretConstants
-import com.google.gson.GsonBuilder
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
@@ -23,9 +21,13 @@ interface NaverAPI {
                 .create(NaverAPI::class.java)
         }
     }
+
     // 지도 검색 API
     @GET("/v1/search/local.json")
-    @Headers("X-Naver-Client-Id: ${SecretConstants.NAVER_CLIENT_ID}", "X-Naver-Client-Secret: ${SecretConstants.NAVER_CLIENT_SECRET}")
+    @Headers(
+        "X-Naver-Client-Id: ${SecretConstants.NAVER_CLIENT_ID}",
+        "X-Naver-Client-Secret: ${SecretConstants.NAVER_CLIENT_SECRET}"
+    )
     fun getMapSearch(
         @Query("query") query: String,
         @Query("display") display: Int,
