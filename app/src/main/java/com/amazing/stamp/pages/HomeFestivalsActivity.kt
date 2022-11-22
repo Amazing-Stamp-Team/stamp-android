0db02ff086b2c3f34412d58930e8b614a014fe43
Yoon <yoon6763@naver.com>
2022-11-22 14:44:34 +0900

    축제 지역 및 시즌 버튼 반복문 이벤트 처리

--- app/src/main/java/com/amazing/stamp/pages/HomeFestivalsActivity.kt
+++ app/src/main/java/com/amazing/stamp/pages/HomeFestivalsActivity.kt
@@ -2,7 +2,9 @@ package com.amazing.stamp.pages
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
 import com.amazing.stamp.utils.ParentActivity
+import com.example.stamp.R
 import com.example.stamp.databinding.ActivityLocalFestivalsBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
@@ -10,13 +12,29 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 
 
-class HomeFestivalsActivity : ParentActivity(){
+class HomeFestivalsActivity : ParentActivity() {
     private val binding by lazy { ActivityLocalFestivalsBinding.inflate(layoutInflater) }
 
     private val storage by lazy { Firebase.storage }
     private val fireStore by lazy { Firebase.firestore }
     private val auth by lazy { Firebase.auth }
-
+    private val seasonCheck by lazy { Array(12) { false } }
+    private var regionBtn = arrayOf<Button?>()
+    private val region = arrayOf(
+        "전국",
+        "서울",
+        "경기",
+        "강원",
+        "충북",
+        "충남",
+        "전북",
+        "전남",
+        "경북",
+        "경남",
+        "부산",
+        "제주"
+    )
+    private val regionCheck by lazy { Array(region.size) { false } }
 
     private val TAG = "TAG_HOMEFESTIVALS"
 
@@ -30,8 +48,65 @@ class HomeFestivalsActivity : ParentActivity(){
         }
 
 
+        // 지역 버튼 클릭 이벤트
+        regionBtn = Array<Button?>(region.size) { null }
+        for(i in region.indices) {
+            regionBtn[i] = findViewById(resources.getIdentifier("btn_attraction_region_$i", "id", packageName))
+            regionBtn[i]?.setOnClickListener {
+                if(i == 0) {
+                    regionCheck[0] = !regionCheck[0]
+                    for(j in 1 until region.size) {
+                        regionCheck[j] = regionCheck[0]
+                    }
+                } else {
+                    regionCheck[i] = !regionCheck[i]
+                    var checkCnt = 0
+                    for (j in 1 until region.size) {
+                        if (regionCheck[j]) {
+                            checkCnt++
+                        }
+                    }
+                    regionCheck[0] = checkCnt == region.size - 1
+                }
+                btnRefresh()
+            }
+        }
 
+
+        // 시즌 버튼 클릭 리스너 설정
+        val seasonBtn = Array<Button?>(12) { null }
+
+        for (i in 0..11) {
+            seasonBtn[i] = findViewById(
+                resources.getIdentifier(
+                    "btn_attraction_season_${i + 1}",
+                    "id",
+                    packageName
+                )
+            )
+
+            seasonBtn[i]?.setOnClickListener {
+                if (seasonCheck[i]) {
+                    seasonCheck[i] = false
+                    seasonBtn[i]?.setBackgroundResource(R.drawable.btn_dark_gray_10)
+                } else {
+                    seasonCheck[i] = true
+                    seasonBtn[i]?.setBackgroundResource(R.drawable.btn_main_10)
+                }
+            }
+        }
     }
+
+    private fun btnRefresh() {
+        for(i in region.indices) {
+            if (regionCheck[i]) {
+                regionBtn[i]?.setBackgroundResource(R.drawable.btn_main_10)
+            } else {
+                regionBtn[i]?.setBackgroundResource(R.drawable.btn_dark_gray_10)
+            }
+        }
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // 앱 바 클릭 이벤트
         when (item.itemId) {
@@ -42,5 +117,4 @@ class HomeFestivalsActivity : ParentActivity(){
         }
         return super.onOptionsItemSelected(item)
     }
-
 }
