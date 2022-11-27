289231378177da8982d7885fc1cb910c92555f4b
Yoon <yoon6763@naver.com>
2022-11-27 14:57:14 +0900

    축제 DTO 추가

---
+++ app/src/main/java/com/amazing/stamp/api/dto/festivalDTO/Header.kt
@@ -0,0 +1,6 @@
+package com.amazing.stamp.api.dto.festivalDTO
+
+data class Header(
+    val resultCode: String,
+    val resultMsg: String
+)
---
+++ app/src/main/java/com/amazing/stamp/api/dto/festivalDTO/Item.kt
@@ -0,0 +1,26 @@
+package com.amazing.stamp.api.dto.festivalDTO
+
+data class Item(
+    val addr1: String,
+    val addr2: String,
+    val areacode: String,
+    val booktour: String,
+    val cat1: String,
+    val cat2: String,
+    val cat3: String,
+    val contentid: String,
+    val contenttypeid: String,
+    val createdtime: String,
+    val eventenddate: String,
+    val eventstartdate: String,
+    val firstimage: String,
+    val firstimage2: String,
+    val mapx: String,
+    val mapy: String,
+    val mlevel: String,
+    val modifiedtime: String,
+    val readcount: Int,
+    val sigungucode: String,
+    val tel: String,
+    val title: String
+)
